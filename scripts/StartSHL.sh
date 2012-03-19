01f881d73417a824daf7cc1a3e5abbcd8e46eec2
Jayce Dowell <jayce.dowell@gmail.com>
2012-03-19 03:01:45 +0000

    Fixed a few typos in the new StartSHL.sh script.

--- scripts/StartSHL.sh
+++ scripts/StartSHL.sh
@@ -7,10 +7,10 @@ case "$1" in
 
   cd /lwa/software
   python /lwa/software/shl_cmnd.py -c /lwa/software/defaults.cfg -d -l /lwa/runtime/runtime.log
+  ;;
 
 'stop')
   pid=`pgrep -f shl_cmnd.py`
   kill -9 $pid
-
-;;
+  ;;
 esac
