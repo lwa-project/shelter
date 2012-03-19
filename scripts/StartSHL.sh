eb3809a44020beb681451f478656e81e55be3c3c
Jayce Dowell <jayce.dowell@gmail.com>
2012-03-19 02:57:58 +0000

    Added a startup script a la DROS to try to get around the shl_cmnd.py auto-start problem on SHL MCS.

---
+++ scripts/StartSHL.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+case "$1" in
+'start')
+  date >> /lwa/runtime/start
+  echo "starting shl_cmnd.py" >> /lwa/runtime/runtime.log
+
+  cd /lwa/software
+  python /lwa/software/shl_cmnd.py -c /lwa/software/defaults.cfg -d -l /lwa/runtime/runtime.log
+
+'stop')
+  pid=`pgrep -f shl_cmnd.py`
+  kill -9 $pid
+
+;;
+esac
