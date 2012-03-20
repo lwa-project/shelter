707bcda6d703160530fd697554a2e1d9f4fd156d
Jayce Dowell <jayce.dowell@gmail.com>
2012-03-20 16:04:13 +0000

    Added stop, restart, and status options to StartSHL.sh to make it a little more user-friendly.

--- scripts/StartSHL.sh
+++ scripts/StartSHL.sh
@@ -2,15 +2,54 @@
 
 case "$1" in
 'start')
+  echo -n "Starting SHL MCS..."
+
   date >> /lwa/runtime/start
   echo "starting shl_cmnd.py" >> /lwa/runtime/runtime.log
 
   cd /lwa/software
   python /lwa/software/shl_cmnd.py -c /lwa/software/defaults.cfg -d -l /lwa/runtime/runtime.log
+
+  echo "done"
   ;;
 
 'stop')
+  echo -n "Stopping SHL MCS..."
+
+  # Find the shl_cmnd.py process
   pid=`pgrep -f shl_cmnd.py`
-  kill -9 $pid
+  if [[ $pid -ne "" ]]; then
+    # If it's running, kill it softly and wait to see what happens
+    kill $pid
+  
+    # Give it up to 30 seconds to comply.  After that...  SIGKILL
+    while [[ $pid -ne "" ]]; do 
+      sleep 5
+      ((t += 5))
+
+      pid=`pgrep -f shl_cmnd.py`
+      if (( t > 30 )); then 
+        kill -s SIGKILL $pid
+      fi
+    done
+  fi
+
+  echo "done"
   ;;
+
+'restart')
+  stop
+  start
+  ;;
+
+'status')
+  # Find the shl_cmnd.py process
+  pid=`pgrep -f shl_cmnd.py`
+  if [[ $pid -ne "" ]]; then
+    echo "SHL MCS appears to be running with PID $pid"
+  else
+    echo "SHL MCS does *not* appear to be running"
+  fi
+  ;;
+
 esac
