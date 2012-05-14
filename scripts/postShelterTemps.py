f0e2bfc3aceaac0f41f7fdd3c41bb02cb6c0a3ec
Jayce Dowell <jayce.dowell@gmail.com>
2012-05-14 22:02:02 +0000

    Cleaned up postShelterTemps.py to send NaNs if nothing has been updated in more than 5 minutes.

--- scripts/postShelterTemps.py
+++ scripts/postShelterTemps.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
+import time
 import urllib
 import subprocess
 
 
+URL = "http://lwalab.phys.unm.edu/OpScreen/test.php"
 KEY = "c0843461abe746a4608dd9c897f9b261"
 SUBSYSTEM = "SHL"
 
-
+# Get the last line of the log file
 t = subprocess.Popen(["tail", "-n1", '/data/thermometer01.txt'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 test, junk = t.communicate()
 test = test.replace('\n', '')
 
-url = "http://lwalab.phys.unm.edu/OpScreen/test.php"
+# Check to see if the log is actually getting updated.  If not, send NaNs
+lastUpdated, junk = test.split(',', 1)
+lastUpdated = float(lastUpdated)
+if time.time() > lastUpdated + 300:
+	test = "%.2f,NaN" % time.time()
 
+# Send the update to lwalab
 p = urllib.urlencode({'key': KEY, 'subsystem': SUBSYSTEM, 'data': test})
 f = urllib.urlopen(url, p)
 print f.read()
