d8a325cf8deaae221e517b40a292982a09d9ea18
Jayce Dowell <jayce.dowell@gmail.com>
2012-11-02 21:15:20 +0000

    Another attempt to deal with the sqlite3 deadlock.

--- shlThreads.py
+++ shlThreads.py
@@ -984,8 +984,10 @@ class Weather(object):
 				while ((tNow % 60) < 10) or ((tNow % 60) > 50):
 					time.sleep(5)
 					tNow = int(time.time())
+				while os.system("lsof %s" % self.database) == 0:
+					time.sleep(5)
 								
-				conn = sqlite3.connect(self.database, timeout=5)
+				conn = sqlite3.connect(self.database, timeout=15)
 				conn.row_factory = sqlite3.Row
 
 				c = conn.cursor()
