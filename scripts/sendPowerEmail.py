394aa52701e29c445c8d418125b6f1495aaeaa8c
Jayce Dowell <jayce.dowell@gmail.com>
2012-12-04 20:06:50 +0000

    Yet another unexcepted behavior in sendPowerEmail.py.

--- scripts/sendPowerEmail.py
+++ scripts/sendPowerEmail.py
@@ -39,16 +39,19 @@ for line in output:
 	fields = line.split(',')
 	if fields[-3] != 'Normal':
 		# Guard against the output power source being None because of failed communications
-		if float(fields[-1]) > 0:
+		if float(fields[-1]) >= 0:
 			powerLoss = True
 			powerLossTime = float(fields[0]) 
 			powerLossReason = "Output power source is '%s'" % fields[-3]
 		else:
 			sys.stderr.write("Output power power is '%s' but the corresponding log entry is:\n%s\n" % (fields[-3], line))
 	elif float(fields[-1]) < 100:
-		powerLoss = True
-		powerLossTime = float(fields[0])
-		powerLossReason = "Battery at %i%%" % float(fields[-1])
+		if float(fields[-1] >= 0:
+			powerLoss = True
+			powerLossTime = float(fields[0])
+			powerLossReason = "Battery at %i%%" % float(fields[-1])
+		else:
+			sys.stderr.write("Battery status is listed as %i%%\n", float(fields[-1]))
 	else:
 		pass
 
