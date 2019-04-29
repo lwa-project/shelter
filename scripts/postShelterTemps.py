5c2be9890873139f6a9a43d6e45eacdfa12e342b
Jayce Dowell <jayce.dowell@gmail.com>
2019-04-29 19:02:45 +0000

    Updated postShelterTemps.py to also report the HVAC status.

--- scripts/postShelterTemps.py
+++ scripts/postShelterTemps.py
@@ -14,21 +14,47 @@ SUBSYSTEM = "SHL"
 
 # Get the last line of the log file
 t = subprocess.Popen(["tail", "-n1", '/data/thermometer01.txt'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-test, junk = t.communicate()
+test, _ = t.communicate()
 test = test.replace('\n', '')
+
+# Get the HVAC status
+try:
+    t = subprocess.Popen(['lead_lag_status',], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    lls, _ = t.communicate()
+    lls = lls.strip().rstrip().split(None, 1)[1]
+    if lls.find('?') != -1:
+        lls = 'NaN'
+        
+    t = subprocess.Popen(['compressor1_status',], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    c1s, _ = t.communicate()
+    c1s = '1' if c1s.find('on') != -1 else c1s
+    c1s = '-1' if c1s.find('disabled') != -1 else c1s
+    c1s = '0' if c1s.find('off') != -1 else c1s
+    
+    t = subprocess.Popen(['compressor2_status',], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    c2s, _ = t.communicate()
+    c2s = '1' if c2s.find('on') != -1 else c2s
+    c2s = '-1' if c2s.find('disabled') != -1 else c2s
+    c2s = '0' if c2s.find('off') != -1 else c2s
+    
+except (OSError, subprocess.CalledProcessError, ValueError):
+    lls = 'NaN'
+    c1s = 'NaN'
+    c2s = 'NaN'
+test += ','+lls+','+c1s+','+c2s
 
 # Check to see if the log is actually getting updated.  If not, send NaNs
 try:
-    lastUpdated, junk = test.split(',', 1)
+    lastUpdated, _ = test.split(',', 1)
 except ValueError:
     lastUpdated = 0.0
-    junk = 0.0
 lastUpdated = float(lastUpdated)
 if time.time() > lastUpdated + 300:
     test = "%.2f,NaN" % time.time()
     if SITE == 'lwasv':
         test += ',NaN'
-
+    test += ',NaN,NaN,NaN'
+    
 # Send the update to lwalab
 p = urllib.urlencode({'key': KEY, 'site': SITE, 'subsystem': SUBSYSTEM, 'data': test})
 f = urllib.urlopen(URL, p)
